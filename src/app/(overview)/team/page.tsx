88387ee07836d8900eb1b310a31da591e34e6e08
Jhonni10 <jhonieripianoscue@gmail.com>
2024-08-30 22:53:08 -0500

    Added BreadCrums, Jobs, and Reviews components to various pages, and updated page layouts with new headings and classes.

--- src/app/(overview)/team/page.tsx
+++ src/app/(overview)/team/page.tsx
@@ -1,8 +1,11 @@
+import { BreadCrums } from "@/components";
 
 export default function TeamPage() {
   return (
-    <div>
-      <h1>TeamPage</h1>
+    <div className="pt-24"  >
+      <BreadCrums/>  
+      <h1 className="text-center text-2xl lg:text-4xl font-semibold pt-6">Nuestro equipo</h1>
+       {/*  Other seccions */}
     </div>
   );
 }
