b8efd265517c8e4a423b3b3f9658eeed2290d079
Jhonni10 <jhonieripianoscue@gmail.com>
2024-09-10 10:46:27 -0500

    Removed unnecessary div elements, updated font styles, and added conditional classnames based on pathname in various components.

--- src/app/(overview)/categories/page.tsx
+++ src/app/(overview)/categories/page.tsx
@@ -4,11 +4,9 @@ export default function CategoriesPage() {
   return (
     <div className="pt-24"  >
       <BreadCrums/>  
-      <h1 className="text-center text-2xl lg:text-4xl font-semibold pt-6">Conoce Nuestras Categorias</h1>
+      <h1 className="text-center text-2xl lg:text-4xl font-bold pt-6 my-4">Conoce Nuestras Categorias</h1>
       <ListCard/>
-      <div className="py-12 sm:p-0">
       <Jobs/>
-      </div>
     </div>
   );
 }
