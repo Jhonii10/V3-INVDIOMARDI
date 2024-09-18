dbd2b8f6f624445511544df3fa8952c90dfd58e9
Jhonni10 <jhonieripianoscue@gmail.com>
2024-09-18 09:21:13 -0500

    Update .hintrc file and Search component to add search functionality with filtering and routing

--- src/components/ui/Search.tsx
+++ src/components/ui/Search.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable jsx-a11y/role-has-required-aria-props */
 'use client'
 import { useUiStore } from '@/store'
-import React, { useEffect } from 'react'
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
 
 export const Search = () => {
     const isSearchOpen = useUiStore((state) => state.isSearchOpen)
@@ -12,8 +13,30 @@ export const Search = () => {
           document.body.classList.add('overflow-hidden');
         } else {
           document.body.classList.remove('overflow-hidden');
+          setQuery('');
         }
       }, [isSearchOpen]);
+
+      const [query, setQuery] = useState('');
+
+    // Definir las rutas manualmente
+    const routes = [
+        { path: '/', name: 'Inicio' },
+        { path: '/about', name: 'Quienes somos' },
+        { path: '/products', name: 'categorias' },
+        { path: '/contact', name: 'Contactanos' },
+        { path: '/jobs', name: 'Oportunidades' },
+        { path: '/locations', name: 'Ubicaciones' },
+        { path: '/reviews', name: 'Reseñas' },
+        { path: '/team', name: 'nuestro equipo' },
+        //...
+    ];
+
+    const filteredRoutes = routes.filter((route) =>
+        route.name.toLowerCase().includes(query.toLowerCase()) ||
+        route.path.toLowerCase().includes(query.toLowerCase())
+    );
+
 
   return (
     <>
@@ -55,6 +78,8 @@ export const Search = () => {
               data-autofocus=""
               defaultValue=""
               style={{ caretColor: "rgb(107, 114, 128)" }}
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
             />
             <svg
               className="pointer-events-none absolute right-4 top-4 h-6 w-6 fill-black"
@@ -63,6 +88,37 @@ export const Search = () => {
               <path d="M20.47 21.53a.75.75 0 1 0 1.06-1.06l-1.06 1.06Zm-9.97-4.28a6.75 6.75 0 0 1-6.75-6.75h-1.5a8.25 8.25 0 0 0 8.25 8.25v-1.5ZM3.75 10.5a6.75 6.75 0 0 1 6.75-6.75v-1.5a8.25 8.25 0 0 0-8.25 8.25h1.5Zm6.75-6.75a6.75 6.75 0 0 1 6.75 6.75h1.5a8.25 8.25 0 0 0-8.25-8.25v1.5Zm11.03 16.72-5.196-5.197-1.061 1.06 5.197 5.197 1.06-1.06Zm-4.28-9.97c0 1.864-.755 3.55-1.977 4.773l1.06 1.06A8.226 8.226 0 0 0 18.75 10.5h-1.5Zm-1.977 4.773A6.727 6.727 0 0 1 10.5 17.25v1.5a8.226 8.226 0 0 0 5.834-2.416l-1.061-1.061Z" />
             </svg>
           </div>
+          {
+            filteredRoutes.length > 0 && query && (
+            <div className='max-h-[18.375rem] divide-y divide-slate-200 overflow-y-auto rounded-b-lg border-t border-slate-200 text-sm leading-6'>
+                {
+
+                    filteredRoutes.map((route , index)=>(
+                        <Link
+                            href={route.path}
+                            key={index}
+                            onClick={closeSearch}
+                        >
+                        <div
+                        className="flex items-center justify-between p-4 hover:bg-slate-50 group"
+                        id="headlessui-combobox-option-:r1gv:"
+                        tabIndex={-1}
+                        data-headlessui-state=""
+                        key={index}
+                        >
+                            <span className="whitespace-nowrap font-semibold text-slate-900 group-hover:text-[var(--primary-color-400)] capitalize  ">
+                                {route.name}
+                            </span>
+                            <span className="ml-4 text-right text-xs text-slate-600">
+                                {route.path}
+                            </span>
+                        </div>
+                        </Link>
+                    ))
+                    
+                }
+            </div>)
+          }
         </div>
       </div>
     </div>
