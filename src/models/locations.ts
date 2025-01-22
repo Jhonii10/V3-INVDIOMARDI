84fc7698552b9311850206a9336714b43204d755
Jhonni10 <jhonieripianoscue@gmail.com>
2025-01-22 13:56:17 -0500

    update locations and UI updates

--- src/models/locations.ts
+++ src/models/locations.ts
@@ -1,79 +1,86 @@
 export const locationsRetailers = [
     {
         tienda:'pasarela',
-        direcion:'calle 1 # 10 -120',
+        direcion:'Calle 24A Norte #4N - 32  Local: 2',
         horarios:'lunes a sabado: 9:00 AM - 8:00 PM , Domingos y festivos: 10:00 AM – 7:00 PM',
-        ubicacion:'https://acortar.link/vrOfjl',
-        telefono:'12345678'
+        ubicacion:'Cl. 24a Nte. #38 - 5, San Vicente, Cali, Valle del Cauca',
+        telefono:'602-4854024'
 
     },
     {
         tienda:'cosmocentro 1',
-        direcion:'calle 1 # 10 -120',
+        direcion:'Calle 5 #50 - 103, C.C. Cosmocentro - Local: 103 & 180.',
         horarios:'lunes a sabado: 9:00 AM - 8:00 PM , Domingos y festivos: 10:00 AM – 7:00 PM',
-        ubicacion:'https://acortar.link/vrOfjl',
-        telefono:'12345678'
+        ubicacion:'Centro Comercial Cosmocentro., Cl. 5 #50 - 103, Cali, Valle del Cauca',
+        telefono:'602-4856175'
 
     },
     {
         tienda:'valle de lili',
-        direcion:'calle 1 # 10 -120',
+        direcion:'Carrera 98B #25 - 130, C.C. La 14 Valle del Lili - Local: 1 - 2',
         horarios:'lunes a sabado: 9:00 AM - 8:00 PM , Domingos y festivos: 10:00 AM – 7:00 PM',
-        ubicacion:'https://acortar.link/vrOfjl',
-        telefono:'12345678'
+        ubicacion:'Carrera 98B #25 - 130, cali, valle del cauca',
+        telefono:'602-3731777'
 
     },
     {
         tienda:'unicentro',
-        direcion:'calle 1 # 10 -120',
+        direcion:'Calle 13 #89 - 240, C.C. Unicentro - Local: 703 - 704',
         horarios:'lunes a sabado: 9:00 AM - 8:00 PM , Domingos y festivos: 10:00 AM – 7:00 PM',
-        ubicacion:'https://acortar.link/vrOfjl',
-        telefono:'12345678'
+        ubicacion:'Calle 13 #89 - 240, cali, valle del cauca',
+        telefono:'602-3087440'
 
     },
     {
         tienda:'chipichape',
-        direcion:'calle 1 # 10 -120',
+        direcion:' Calle 38 Norte #6N - 45, C.C. Chipichape - Local: 3.17',
         horarios:'lunes a sabado: 9:00 AM - 8:00 PM , Domingos y festivos: 10:00 AM – 7:00 PM',
-        ubicacion:'https://acortar.link/vrOfjl',
-        telefono:'12345678'
+        ubicacion:'Calle 38 Norte #6N - 45, Cali, Valle del cauca',
+        telefono:'602-3744449'
 
     },
     {
         tienda:'cosmocentro 2',
-        direcion:'calle 1 # 10 -120',
+        direcion:'Calle 5 #50 - 103, C.C. Cosmocentro - Local: 103 & 180',
         horarios:'lunes a sabado: 9:00 AM - 8:00 PM , Domingos y festivos: 10:00 AM – 7:00 PM',
-        ubicacion:'https://acortar.link/vrOfjl',
-        telefono:'12345678'
+        ubicacion:'Centro Comercial Cosmocentro., Cl. 5 #50 - 103, Cali, Valle del Cauca',
+        telefono:'602-4855079'
 
     },
     {
         tienda:'calima nuevo',
-        direcion:'calle 1 # 10 -120',
+        direcion:' Calle 70 #1 - 00, C.C. Calima',
         horarios:'lunes a sabado: 9:00 AM - 8:00 PM , Domingos y festivos: 10:00 AM – 7:00 PM',
-        ubicacion:'https://acortar.link/vrOfjl',
-        telefono:'12345678'
+        ubicacion:' Calle 70 #1 - 00, cali, valle del cauca',
+        telefono:'602-4890803'
 
     },
-    
+    {
+        tienda:'Alameda',
+        direcion:'Carrera 23C # 8A - 37',
+        horarios:'lunes a sabado: 9:00 AM - 8:00 PM , Domingos y festivos: 10:00 AM – 7:00 PM',
+        ubicacion:'Carrera 23C # 8A - 37, cali, valle del cauca',
+        telefono:'602-3489545'
+
+    },
 ]
 
 
 export const locationsWholesalers = [
     {
         tienda:'Paseo de la quinta',
-        direcion:'Calle 5',
+        direcion:'Calle 5 #46 - 83, C.C. Paseo de la Quinta - Local: 239 - 240',
         horarios:'lunes a sabado: 8:00 AM - 6:00 PM',
-        ubicacion:'https://acortar.link/vrOfjl',
-        telefono:'12345678'
+        ubicacion:'Centro Comercial Paseo de la Quinta, Cl. 5 #46-83, Cali, Valle del Cauca',
+        telefono:'602-5521230'
 
     },
     {
         tienda:'Bodega principal',
-        direcion:'Cra 36 # 10-140 acopi',
+        direcion:'Cra 36 # 10-140, Acopi, Yumbo, Bodega 10',
         horarios:'lunes a viernes: 7:00 AM - 5:00 PM ',
-        ubicacion:'https://acortar.link/vrOfjl',
-        telefono:'12345678'
+        ubicacion:'Cra. 36 #10-140, Acopi, Yumbo, Valle del Cauca',
+        telefono:'602-5521230'
 
     },
 ]
